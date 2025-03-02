fcf4eecfcb6c1f3e7ef4866c04d674ee147a524a
WeiminXiong <1207959885@qq.com>
2025-03-02 16:51:12 +0000

    update

---
+++ download_data.sh
@@ -0,0 +1,11 @@
+# Download the data
+gdown https://drive.google.com/uc?id=1hV7EQcWWaWNErTCBbGAltqBMwQGcTe0E
+gdown https://drive.google.com/uc?id=1aDp4akN3PK-pMfe0kET6d-eVvcqK2ouy
+
+# Unzip the data
+tar -xzf data.tar.gz
+tar -xzf env_libs.tar.gz
+
+# Remove the zip files
+rm data.tar.gz
+rm env_libs.tar.gz
